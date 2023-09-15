53d47d4b82508269f14ba280704f4a54ed58bda7
Hoa Pham <hoa.phamlethai@gmail.com>
2023-09-15 09:09:50 -0700

    rss_feed_reader: add documentation

--- rss_feed_reader/runner.py
+++ rss_feed_reader/runner.py
@@ -1,11 +1,45 @@
+"""
+RSS Feed Reader
+----------------
+
+This script allows users to fetch and display the latest entry from a list of 
+RSS feeds provided in a JSON file. Each feed is fetched concurrently, making the 
+process faster when dealing with multiple feeds.
+
+Expected JSON Format:
+{
+    "FeedAuthor1": "https://rss.feed.url/1",
+    "FeedAuthor2": "https://rss.feed.url/2",
+    ...
+}
+
+Usage:
+python script_name.py -i path_to_json_file.json
+
+Dependencies:
+- aiohttp
+- asyncio
+- feedparser
+"""
+
 import argparse
 import aiohttp
 import asyncio
 import feedparser
 import json
-import requests
 
 async def fetch_feed(session, feed_url, timeout_duration=10):
+    """
+    Fetch content from the RSS feed URL.
+
+    Args:
+    - session: aiohttp ClientSession for making HTTP requests.
+    - feed_url (str): URL of the RSS feed.
+    - timeout_duration (int, optional): Duration in seconds before timing out the request. Defaults to 10 seconds.
+
+    Returns:
+    - str: Raw content of the RSS feed if fetched successfully, None otherwise.
+    """
     try:
         async with session.get(feed_url, timeout=timeout_duration) as response:
             if response.status == 200:
@@ -19,6 +53,16 @@ async def fetch_feed(session, feed_url, timeout_duration=10):
     return None
 
 async def get_latest_entry(feed_url, timeout_duration=10):
+    """
+    Fetch and parse the latest entry from the RSS feed URL.
+
+    Args:
+    - feed_url (str): URL of the RSS feed.
+    - timeout_duration (int, optional): Duration in seconds before timing out the request. Defaults to 10 seconds.
+
+    Returns:
+    - feedparser.FeedParserDict: Parsed latest entry from the RSS feed.
+    """
     async with aiohttp.ClientSession() as session:
         content = await fetch_feed(session, feed_url, timeout_duration)
         if content:
@@ -28,6 +72,12 @@ async def get_latest_entry(feed_url, timeout_duration=10):
     return None
 
 async def parse_rss_feeds(rss_feeds):
+    """
+    Concurrently fetch and display the latest entries from a list of RSS feeds.
+
+    Args:
+    - rss_feeds (dict): Dictionary with feed authors as keys and RSS feed URLs as values.
+    """
     tasks = [get_latest_entry(feed_url) for feed_url in rss_feeds.values()]
 
     results = await asyncio.gather(*tasks)
@@ -45,7 +95,7 @@ async def parse_rss_feeds(rss_feeds):
 
 def main():
     parser = argparse.ArgumentParser(description='Get first article from each RSS feed')
-    parser.add_argument('-i', '--input', required=True, help='JSON file contains RSS Feeds and their authors')
+    parser.add_argument('-i', '--input', required=True, help='JSON file containing RSS Feeds and their authors')
     args = parser.parse_args()
 
     try:
@@ -54,7 +104,7 @@ def main():
     except FileNotFoundError:
         print(f"Input file not found: {args.input}")
     except Exception as e:
-        print(f"Encounter error for input: {args.input}, err: {e}")
+        print(f"Encountered an error for input: {args.input}. Error: {e}")
 
 
 if __name__ == "__main__":
