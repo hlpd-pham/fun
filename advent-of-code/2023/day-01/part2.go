b6882ce50ace52016af375d525f3cb0ae9452c5d
Hoa Pham <hoa.phamlethai@gmail.com>
2023-12-29 08:40:17 -0800

    finish day 1 part 2 golang

--- advent-of-code/2023/day-01/part2.go
+++ advent-of-code/2023/day-01/part2.go
@@ -9,6 +9,10 @@ import (
   "strconv"
 )
 
+const (
+  BAD_CANDIDATE = "BAD_CANDIDATE"
+)
+
 func main() {
   var inputFile string
   flag.StringVar(&inputFile, "i", "", "Specify the input filename")
@@ -47,7 +51,43 @@ func main() {
   }
 }
 
+// check if candidate is good and return the candidate word, then find the corresponding mapping
+func getCandidate(line string, startIndex int) string {
+  digitWords := map[string]int{
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+  }
+  lineLength := len(line)
+  for key, _ := range digitWords {
+    if startIndex + len(key) - 1 < lineLength {
+      digitWordCandidate := line[startIndex:startIndex+len(key)]
+      if digitWordCandidate == key {
+        return key
+      }
+    }
+  }
+  return BAD_CANDIDATE
+}
+
 func solve(lines []string) error {
+  digitWords := map[string]int{
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+  }
   var total = 0;
   for _, l := range lines {
     var startDigit int
@@ -58,20 +98,32 @@ func solve(lines []string) error {
       if '0' <= l[i] && l[i] <= '9' {
         startDigit, _ = strconv.Atoi(l[i:i+1])
         break
+      } 
+      result := getCandidate(l, i)
+      if result != BAD_CANDIDATE {
+        startDigit = digitWords[result]
+        break
       }
     }
     if startDigit == 0 {
       errorMessage := fmt.Sprintf("cannot find a digit from line: %s", l)
       return errors.New(errorMessage)
     }
+
+    // get second digit
     for i := len(l) - 1; i >= 0; i-- {
       if '0' <= l[i] && l[i] <= '9' {
         endDigit, _ = strconv.Atoi(l[i:i+1])
         break
       }
+      result := getCandidate(l, i)
+      if result != BAD_CANDIDATE {
+        endDigit = digitWords[result]
+        break
+      }
     }
 
-    total += startDigit * 10 + endDigit
+    total += (startDigit * 10 + endDigit)
   }
   fmt.Println(total)
   return nil
