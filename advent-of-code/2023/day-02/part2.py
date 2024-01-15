13be810dd37531406698c16e71ddd48e24fab942
Hoa Pham <hoa.phamlethai@gmail.com>
2024-01-15 15:00:09 -0800

    fix commit day 2 part 2 python

--- advent-of-code/2023/day-02/part2.py
+++ advent-of-code/2023/day-02/part2.py
@@ -7,10 +7,6 @@ import re
 import math
 
 def solve(lines):
-    '''
-    get all lines, stripped, check if each turn has more than the amount of cubes we have from threshold,
-    exit if we do, increment the total if we exit inner loop normally
-    '''
     total = 0
     for id, game in enumerate(lines, start=1):
         threshold = {'red': 0, 'green': 0, 'blue': 0}
