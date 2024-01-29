640841676bf1601ba8239f702041fbf440f8369b
Hoa Pham <hoa.phamlethai@gmail.com>
2024-01-29 08:08:28 -0800

    Create cc activity report

---
+++ subscription_monitor/activity.py
@@ -0,0 +1,60 @@
+import re
+import argparse
+import pandas as pd
+
+AMEX = 'amex'
+CHASE = 'chase'
+ALLOWED_CC_TYPES = [AMEX, CHASE]
+ROW_SEPARATORS = '------------------------------------------------------------'
+
+def validate_cc_type(cc_type):
+    if cc_type not in ALLOWED_CC_TYPES:
+        raise ValueError('cc type must be amex or chase')
+
+def create_amex_report(df):
+
+    pattern = r'MOBILE PAYMENT - THANK YOU'
+    mask = df.Description.str.contains(pattern, regex = True)
+    df_filtered = df[~mask]
+    large = df_filtered[df_filtered.Amount > 300]
+    monthly_spend = df_filtered[df_filtered.Amount <= 300]
+
+    print(f'{ROW_SEPARATORS}\nLarge Transactions (> 300): {sum(large.Amount)}')
+    print(large)
+    print(f'{ROW_SEPARATORS}\nMonthly Spending (<= 300): {sum(monthly_spend.Amount)}')
+    print(monthly_spend)
+
+
+def crease_chase_report(df):
+    columns = ['Transaction Date', 'Description', 'Amount']
+    df = df[columns]
+    df.loc[:, 'Amount'] = df.Amount.abs()
+    chase_pattern = r'Payment Thank You-Mobile'
+    chase_mask = df.Description.str.contains(chase_pattern, regex=True)
+    chase_filtered = df[~chase_mask]
+    costco = chase_filtered[chase_filtered.Description.str.contains("COSTCO")]
+    amzn = chase_filtered[chase_filtered.Description.str.contains("AMZN")]
+
+    print(f'{ROW_SEPARATORS}\nCOSTCO: {sum(costco.Amount)}')
+    print(costco)
+    print(f'{ROW_SEPARATORS}\nAMZN: {sum(amzn.Amount)}')
+    print(amzn)
+
+
+def create_report(csv_file, cc_type):
+    validate_cc_type(cc_type)
+    df = pd.read_csv(csv_file)
+    print('Report Type:', cc_type.upper())
+    if cc_type == AMEX:
+        create_amex_report(df)
+    else:
+        crease_chase_report(df)
+
+if __name__ == "__main__":
+    # Argument parser setup
+    parser = argparse.ArgumentParser(description="Run spending activity repot")
+    parser.add_argument("-c", "--csv", help="CSV input file", type=str, required=True)
+    parser.add_argument("-t", "--type", help="CC provider type", type=str, required=True, choices=ALLOWED_CC_TYPES)
+    args = parser.parse_args()
+
+    create_report(args.csv, args.type)
