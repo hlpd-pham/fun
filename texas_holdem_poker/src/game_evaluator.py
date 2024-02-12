933917a74769b67b1b6ca474a769b647802d357d
Hoa Pham <hoa.phamlethai@gmail.com>
2024-02-12 12:46:16 -0800

    finish game evaluator unit tests

--- texas_holdem_poker/src/game_evaluator.py
+++ texas_holdem_poker/src/game_evaluator.py
@@ -3,73 +3,133 @@ from collections import defaultdict
 from typing import List
 
 from card import Card
+from utils.strings import to_string
+
 
 class GameEvaluator:
-    '''This class always assume player cards have 7 cards to make it simple'''
+    """This class always assume player cards have 7 cards to make it simple"""
 
     @classmethod
     def get_pair_cards(cls, all_player_cards: List[Card]) -> List[Card]:
-        value_cards_map = defaultdict(list)
         logging.info(f"player cards: {[str(card) for card in all_player_cards]}")
+        value_cards_map = defaultdict(list)
         for c in all_player_cards:
             value_cards_map[c.value].append(c)
             if len(value_cards_map[c.value]) == 2:
+                logging.info(f"returning pair: {to_string(value_cards_map[c.value])}")
                 return value_cards_map[c.value]
+        logging.info(
+            f"no pair cards found, value_cards_map: {to_string(value_cards_map)}"
+        )
         return []
 
     @classmethod
     def get_two_pair_cards(cls, all_player_cards: List[Card]) -> List[Card]:
+        logging.info(f"player cards: {[str(card) for card in all_player_cards]}")
         hand_cards = sorted(all_player_cards.copy())
         first_pair_cards = GameEvaluator.get_pair_cards(hand_cards)
         if len(first_pair_cards) == 0:
+            logging.info("no first pair found: returning")
             return []
         for card in first_pair_cards:
             hand_cards.remove(card)
         second_pair_cards = GameEvaluator.get_pair_cards(hand_cards)
         if len(second_pair_cards) == 0:
+            logging.info("no second pair found: returning")
             return []
-        return first_pair_cards + second_pair_cards
+        two_pairs = first_pair_cards + second_pair_cards
+        logging.info(f"two pairs found {to_string(two_pairs)}")
+        return two_pairs
 
     @classmethod
     def get_three_of_a_kind_cards(cls, all_player_cards: List[Card]) -> List[Card]:
+        logging.info(f"player cards: {[str(card) for card in all_player_cards]}")
         value_cards_map = defaultdict(list)
         for c in all_player_cards:
             value_cards_map[c.value].append(c)
             if len(value_cards_map[c.value]) == 3:
+                logging.info(
+                    f"found three of a kind: {to_string(value_cards_map[c.value])}"
+                )
                 return value_cards_map[c.value]
+        logging.info(
+            f"no three of a kind cards found, value_cards_map: {to_string(value_cards_map)}"
+        )
         return []
 
     @classmethod
-    def get_straight_cards(cls, all_player_cards: List[Card]) -> List[Card]:
+    def get_straight_cards(
+        cls, all_player_cards: List[Card], get_all_cards=False
+    ) -> List[Card]:
         """
-        only return the highest straight cards
+        only return the highest straight cards if not indicated otherwise
         """
+        logging.info(
+            f"player cards: {[str(card) for card in all_player_cards]}, get_all_cards: {get_all_cards}"
+        )
         hand_cards = all_player_cards.copy()
         unique_cards = {}
         for card in hand_cards:
             unique_cards[card.value] = card
         sorted_cards = sorted(unique_cards.values())
-        for straight_end_idx in range(len(sorted_cards)-1, len(sorted_cards)-4, -1):
+        straight_result = []
+        logging.info(f"sorted cards {to_string(sorted_cards)}")
+
+        if get_all_cards:
+            left = 0
+            for right in range(1, len(sorted_cards)):
+                if not sorted_cards[right - 1].value + 1 == sorted_cards[right].value:
+                    left = right
+            if right - left + 1 < 5:
+                logging.info("no straight cards found")
+                return []
+            result = sorted_cards[left : right + 1]
+            # check broadway
+            if sorted_cards[-1].value == 13 and sorted_cards[0].value == 1:
+                result.append(sorted_cards[0])
+            logging.info(f"all straight cards found: {to_string(result)}")
+            return result
+
+        for straight_end_idx in range(len(sorted_cards) - 1, len(sorted_cards) - 4, -1):
             straight_start_idx = straight_end_idx - 4
             found_straight = True
             for i in range(straight_start_idx, straight_end_idx):
-                if not(sorted_cards[i].value + 1 == sorted_cards[i+1].value):
+                if not (sorted_cards[i].value + 1 == sorted_cards[i + 1].value):
                     found_straight = False
                     break
             if found_straight:
-                return sorted_cards[straight_start_idx:straight_end_idx+1]
+                straight_result = sorted_cards[
+                    straight_start_idx : straight_end_idx + 1
+                ]
+                logging.info(f"found_straight: {to_string(straight_result)}")
+                break
 
-        potential_broadway_values = [sorted_cards[0].value] + [card.value for card in sorted_cards[-4:]]
-        if potential_broadway_values == [1,10,11,12,13]:
-            return [sorted_cards[0]] + sorted_cards[-4:]
+        
+        # check broadway
+        potential_broadway_values = [sorted_cards[0].value] + [
+            card.value for card in sorted_cards[-4:]
+        ]
+        if potential_broadway_values == [1, 10, 11, 12, 13]:
+            broadway = [sorted_cards[0]] + sorted_cards[-4:]
+            logging.info(f"found broadway straight: {to_string(broadway)}")
+            return broadway
+        if straight_result:
+            logging.info(f"found straight: {to_string(straight_result)}")
+            return straight_result
 
+        logging.info(f"no straight found, returning")
         return []
 
     @classmethod
-    def get_flush_cards(cls, all_player_cards: List[Card]) -> List[Card]:
+    def get_flush_cards(
+        cls, all_player_cards: List[Card], get_all_cards=False
+    ) -> List[Card]:
         """
         only return the highest flush cards
         """
+        logging.info(
+            f"player cards: {[str(card) for card in all_player_cards]}, get_all_cards: {get_all_cards}"
+        )
         hand_cards = sorted(all_player_cards)
         suite_cards_map = defaultdict(list)
         flush_suite = None
@@ -79,48 +139,94 @@ class GameEvaluator:
                 flush_suite = card.suite
 
         if flush_suite:
-            return suite_cards_map[flush_suite][-5:]
+            flush_cards = suite_cards_map[flush_suite]
+            result = sorted(flush_cards, key=lambda card: card.get_high_card_value())
+            if get_all_cards:
+                logging.info(f"returning all flush cards: {to_string(result)}")
+                return result
+            result = result[-5:]
+            logging.info(f"returning 5 highest flush cards: {to_string(result)}")
+            return result
 
+        logging.info(f"no flush cards found")
         return []
 
     @classmethod
     def get_full_house_cards(cls, all_player_cards: List[Card]) -> List[Card]:
+        logging.info(f"player cards: {[str(card) for card in all_player_cards]}")
         hand_cards = all_player_cards.copy()
         three_of_a_kind_cards = cls.get_three_of_a_kind_cards(hand_cards)
         if len(three_of_a_kind_cards) == 0:
+            logging.info(f"no three of a kind in full house hand")
             return []
+        logging.info(
+            f"three of a kind found in full house hand: {to_string(three_of_a_kind_cards)}"
+        )
         for c in three_of_a_kind_cards:
             hand_cards.remove(c)
-        pair_cards = cls.get_pair_cards(hand_cards)
-        if len(pair_cards) == 0:
+        first_pair = cls.get_pair_cards(hand_cards)
+        if len(first_pair) == 0:
+            logging.info(f"no pair in full house hand")
             return []
-        return three_of_a_kind_cards + pair_cards
+        logging.info(f"first pair found in full house hand: {to_string(first_pair)}")
+        for c in first_pair:
+            hand_cards.remove(c)
+        second_pair = cls.get_pair_cards(hand_cards)
+        logging.info(f"second pair found in full house hand: {to_string(second_pair)}")
+
+        result = []
+        if second_pair and second_pair[0] > first_pair[0]:
+            result = three_of_a_kind_cards + second_pair
+        else:
+            result = three_of_a_kind_cards + first_pair
+        logging.info(f"full house hand found: {to_string(result)}")
+        return result
 
     @classmethod
     def get_four_of_a_kind_cards(cls, all_player_cards: List[Card]) -> List[Card]:
+        logging.info(f"player cards: {[str(card) for card in all_player_cards]}")
         value_cards_map = defaultdict(list)
         for c in all_player_cards:
             value_cards_map[c.value].append(c)
             if len(value_cards_map[c.value]) == 4:
+                logging.info(
+                    f"found four of a kind: {to_string(value_cards_map[c.value])}"
+                )
                 return value_cards_map[c.value]
+        logging.info(
+            f"no four of a kind cards found, value_cards_map: {to_string(value_cards_map)}"
+        )
         return []
 
     @classmethod
     def get_straight_flush_cards(cls, all_player_cards: List[Card]) -> List[Card]:
-        straight_cards = sorted(
-            GameEvaluator.get_straight_cards(all_player_cards),
-            key=lambda card: (card.value, card.suite),
+        logging.info(f"player cards: {[str(card) for card in all_player_cards]}")
+        straight_cards = GameEvaluator.get_straight_cards(
+            all_player_cards, get_all_cards=True
         )
-        flush_cards = sorted(
-            GameEvaluator.get_flush_cards(all_player_cards),
-            key=lambda card: (card.value, card.suite),
-        )
-        if straight_cards != flush_cards:
+        if not straight_cards:
+            logging.info("no straight cards for straight flush hand")
             return []
-        return straight_cards
+        straight_flush_cards = GameEvaluator.get_flush_cards(
+            straight_cards, get_all_cards=True
+        )
+
+        if not straight_flush_cards:
+            logging.info("no flush cards for straight flush hand")
+            return []
+        # check royal and smallest straight
+        if straight_flush_cards[-1].value == 1 and straight_flush_cards[-2].value in (13,5):
+            straight_flush_cards = straight_flush_cards[-5:]
+        else:
+            start_index = max(0, len(straight_flush_cards)-1- 5)
+            # Slice the array from start_index to target_index
+            straight_flush_cards = straight_flush_cards[start_index:-1]
+        logging.info(f"straight flush cards result: {to_string(straight_flush_cards)}")
+        return straight_flush_cards
 
     @classmethod
     def get_royal_flush_cards(cls, all_player_cards: List[Card]) -> List[Card]:
+        logging.info(f"player cards: {[str(card) for card in all_player_cards]}")
         straight_flush_cards = GameEvaluator.get_straight_flush_cards(all_player_cards)
         straight_flush_card_values = set([c.value for c in straight_flush_cards])
 
--- texas_holdem_poker/tests/test_game_evaluator.py
+++ texas_holdem_poker/tests/test_game_evaluator.py
@@ -1,5 +1,7 @@
+import logging
 from card import Card, Suite
 from game_evaluator import GameEvaluator
+from utils.strings import to_string
 
 
 def test_get_one_pair_happy_path_return_2_cards():
@@ -44,8 +46,9 @@ def test_two_pairs_happy_path_return_4_cards():
     result = GameEvaluator.get_two_pair_cards(hand_cards)
     assert len(result) == 4
 
+
 def test_two_pairs_happy_path_return_0_cards():
-    '''only 1 pair'''
+    """only 1 pair"""
     hand_cards = [
         Card(1, Suite.HEART),
         Card(1, Suite.CLUB),
@@ -58,8 +61,9 @@ def test_two_pairs_happy_path_return_0_cards():
     result = GameEvaluator.get_two_pair_cards(hand_cards)
     assert len(result) == 0
 
+
 def test_two_pairs_happy_path_return_0_cards_3_of_a_kind():
-    '''only 1 pair'''
+    """only 1 pair"""
     hand_cards = [
         Card(1, Suite.HEART),
         Card(1, Suite.CLUB),
@@ -71,6 +75,7 @@ def test_two_pairs_happy_path_return_0_cards_3_of_a_kind():
     ]
     result = GameEvaluator.get_two_pair_cards(hand_cards)
     assert len(result) == 0
+
 
 def test_two_pairs_sad_path_0_pair():
     hand_cards = [
@@ -85,8 +90,9 @@ def test_two_pairs_sad_path_0_pair():
     result = GameEvaluator.get_two_pair_cards(hand_cards)
     assert len(result) == 0
 
+
 def test_three_of_a_kind_happy_path():
-    '''classic happy path hand'''
+    """classic happy path hand"""
     hand_cards = [
         Card(3, Suite.HEART),
         Card(3, Suite.CLUB),
@@ -98,6 +104,7 @@ def test_three_of_a_kind_happy_path():
     ]
     result = GameEvaluator.get_three_of_a_kind_cards(hand_cards)
     assert len(result) == 3
+
 
 def test_three_of_a_kind_sad_path():
     hand_cards = [
@@ -111,6 +118,7 @@ def test_three_of_a_kind_sad_path():
     ]
     result = GameEvaluator.get_three_of_a_kind_cards(hand_cards)
     assert len(result) == 0
+
 
 def test_straight_happy_path_normal():
     hand_cards = [
@@ -127,10 +135,11 @@ def test_straight_happy_path_normal():
         Card(4, Suite.CLUB),
         Card(5, Suite.SPADE),
         Card(6, Suite.CLUB),
-        Card(7, Suite.HEART)
+        Card(7, Suite.HEART),
     ]
     result = GameEvaluator.get_straight_cards(hand_cards)
     assert expected == result
+
 
 def test_straight_happy_path_higher_straight():
     hand_cards = [
@@ -152,6 +161,7 @@ def test_straight_happy_path_higher_straight():
     result = GameEvaluator.get_straight_cards(hand_cards)
     assert expected == result
 
+
 def test_straight_happy_path_broadway():
     hand_cards = [
         Card(1, Suite.HEART),
@@ -171,6 +181,27 @@ def test_straight_happy_path_broadway():
     ]
     result = GameEvaluator.get_straight_cards(hand_cards)
     assert expected == result
+
+def test_straight_happy_path_broadway_7_cards():
+    hand_cards = [
+        Card(1, Suite.HEART),
+        Card(8, Suite.CLUB),
+        Card(9, Suite.SPADE),
+        Card(10, Suite.CLUB),
+        Card(11, Suite.HEART),
+        Card(12, Suite.HEART),
+        Card(13, Suite.HEART),
+    ]
+    expected = [
+        Card(1, Suite.HEART),
+        Card(10, Suite.CLUB),
+        Card(11, Suite.HEART),
+        Card(12, Suite.HEART),
+        Card(13, Suite.HEART),
+    ]
+    result = GameEvaluator.get_straight_cards(hand_cards)
+    assert expected == result
+
 
 def test_straight_happy_path_smallest_straight():
     hand_cards = [
@@ -192,6 +223,7 @@ def test_straight_happy_path_smallest_straight():
     result = GameEvaluator.get_straight_cards(hand_cards)
     assert expected == result
 
+
 def test_straight_sad_path_no_straight():
     hand_cards = [
         Card(1, Suite.HEART),
@@ -202,7 +234,297 @@ def test_straight_sad_path_no_straight():
         Card(12, Suite.HEART),
         Card(13, Suite.HEART),
     ]
+    expected = []
+    result = GameEvaluator.get_straight_cards(hand_cards)
+    assert expected == result
+
+
+def test_flush_happy_path_normal():
+    hand_cards = [
+        Card(1, Suite.HEART),
+        Card(2, Suite.HEART),
+        Card(3, Suite.HEART),
+        Card(4, Suite.HEART),
+        Card(5, Suite.HEART),
+        Card(4, Suite.SPADE),
+        Card(5, Suite.SPADE),
+    ]
+    expected = [
+        Card(2, Suite.HEART),
+        Card(3, Suite.HEART),
+        Card(4, Suite.HEART),
+        Card(5, Suite.HEART),
+        Card(1, Suite.HEART),
+    ]
+    result = GameEvaluator.get_flush_cards(hand_cards)
+    assert expected == result
+
+
+def test_flush_happy_path_6_cards_flush():
+    hand_cards = [
+        Card(1, Suite.HEART),
+        Card(2, Suite.HEART),
+        Card(3, Suite.HEART),
+        Card(4, Suite.HEART),
+        Card(5, Suite.HEART),
+        Card(13, Suite.HEART),
+        Card(5, Suite.SPADE),
+    ]
+    expected = [
+        Card(3, Suite.HEART),
+        Card(4, Suite.HEART),
+        Card(5, Suite.HEART),
+        Card(13, Suite.HEART),
+        Card(1, Suite.HEART),
+    ]
+    result = GameEvaluator.get_flush_cards(hand_cards)
+    assert expected == result
+
+
+def test_flush_sad_path():
+    hand_cards = [
+        Card(1, Suite.HEART),
+        Card(2, Suite.DIAMOND),
+        Card(3, Suite.CLUB),
+        Card(4, Suite.HEART),
+        Card(5, Suite.HEART),
+        Card(4, Suite.SPADE),
+        Card(5, Suite.SPADE),
+    ]
+    expected = []
+    result = GameEvaluator.get_flush_cards(hand_cards)
+    assert expected == result
+
+
+def test_full_house_happy_path_one_pair():
+    hand_cards = [
+        Card(1, Suite.HEART),
+        Card(1, Suite.DIAMOND),
+        Card(1, Suite.CLUB),
+        Card(2, Suite.HEART),
+        Card(2, Suite.HEART),
+        Card(4, Suite.SPADE),
+        Card(5, Suite.SPADE),
+    ]
+    expected = [
+        Card(1, Suite.HEART),
+        Card(1, Suite.DIAMOND),
+        Card(1, Suite.CLUB),
+        Card(2, Suite.HEART),
+        Card(2, Suite.HEART),
+    ]
+    result = GameEvaluator.get_full_house_cards(hand_cards)
+    assert expected == result
+
+
+def test_full_house_happy_path_two_pairs():
+    hand_cards = [
+        Card(1, Suite.HEART),
+        Card(1, Suite.DIAMOND),
+        Card(1, Suite.CLUB),
+        Card(2, Suite.HEART),
+        Card(2, Suite.HEART),
+        Card(3, Suite.SPADE),
+        Card(3, Suite.CLUB),
+    ]
+    expected = [
+        Card(1, Suite.HEART),
+        Card(1, Suite.DIAMOND),
+        Card(1, Suite.CLUB),
+        Card(3, Suite.SPADE),
+        Card(3, Suite.CLUB),
+    ]
+    result = GameEvaluator.get_full_house_cards(hand_cards)
+    assert expected == result
+
+
+def test_full_house_sad_path_no_trips():
+    hand_cards = [
+        Card(1, Suite.HEART),
+        Card(1, Suite.DIAMOND),
+        Card(8, Suite.CLUB),
+        Card(2, Suite.HEART),
+        Card(2, Suite.HEART),
+        Card(4, Suite.SPADE),
+        Card(5, Suite.SPADE),
+    ]
+    expected = []
+    result = GameEvaluator.get_full_house_cards(hand_cards)
+    assert expected == result
+
+
+def test_full_house_sad_path_has_trip_no_pair():
+    hand_cards = [
+        Card(1, Suite.HEART),
+        Card(1, Suite.DIAMOND),
+        Card(1, Suite.CLUB),
+        Card(2, Suite.HEART),
+        Card(8, Suite.HEART),
+        Card(4, Suite.SPADE),
+        Card(5, Suite.SPADE),
+    ]
+    expected = []
+    result = GameEvaluator.get_full_house_cards(hand_cards)
+    assert expected == result
+
+
+def test_four_of_a_kind_happy_path():
+    hand_cards = [
+        Card(1, Suite.HEART),
+        Card(1, Suite.DIAMOND),
+        Card(1, Suite.CLUB),
+        Card(1, Suite.SPADE),
+        Card(8, Suite.HEART),
+        Card(4, Suite.SPADE),
+        Card(5, Suite.SPADE),
+    ]
+    expected = [
+        Card(1, Suite.HEART),
+        Card(1, Suite.DIAMOND),
+        Card(1, Suite.CLUB),
+        Card(1, Suite.SPADE),
+    ]
+    result = GameEvaluator.get_four_of_a_kind_cards(hand_cards)
+    assert expected == result
+
+
+def test_four_of_a_kind_sad_path():
+    hand_cards = [
+        Card(1, Suite.HEART),
+        Card(1, Suite.DIAMOND),
+        Card(1, Suite.CLUB),
+        Card(2, Suite.SPADE),
+        Card(8, Suite.HEART),
+        Card(4, Suite.SPADE),
+        Card(5, Suite.SPADE),
+    ]
+    expected = []
+    result = GameEvaluator.get_four_of_a_kind_cards(hand_cards)
+    assert expected == result
+
+
+def test_straight_flush_happy_path():
+    hand_cards = [
+        Card(1, Suite.HEART),
+        Card(2, Suite.HEART),
+        Card(3, Suite.HEART),
+        Card(4, Suite.HEART),
+        Card(5, Suite.HEART),
+        Card(6, Suite.SPADE),
+        Card(7, Suite.SPADE),
+    ]
+    expected = [
+        Card(2, Suite.HEART),
+        Card(3, Suite.HEART),
+        Card(4, Suite.HEART),
+        Card(5, Suite.HEART),
+        Card(1, Suite.HEART),
+    ]
+    result = GameEvaluator.get_straight_flush_cards(hand_cards)
+    assert expected == result
+
+
+def test_straight_flush_happy_path_higher_straight():
+    hand_cards = [
+        Card(1, Suite.HEART),
+        Card(2, Suite.HEART),
+        Card(3, Suite.HEART),
+        Card(4, Suite.HEART),
+        Card(5, Suite.HEART),
+        Card(6, Suite.HEART),
+        Card(7, Suite.SPADE),
+    ]
+    expected = [
+        Card(2, Suite.HEART),
+        Card(3, Suite.HEART),
+        Card(4, Suite.HEART),
+        Card(5, Suite.HEART),
+        Card(6, Suite.HEART),
+    ]
+    result = GameEvaluator.get_straight_flush_cards(hand_cards)
+    assert expected == result
+
+def test_straight_flush_happy_path_royal():
+    hand_cards = [
+        Card(1, Suite.HEART),
+        Card(10, Suite.HEART),
+        Card(11, Suite.HEART),
+        Card(12, Suite.HEART),
+        Card(13, Suite.HEART),
+        Card(8, Suite.HEART),
+        Card(9, Suite.HEART),
+    ]
+    expected = [
+        Card(10, Suite.HEART),
+        Card(11, Suite.HEART),
+        Card(12, Suite.HEART),
+        Card(13, Suite.HEART),
+        Card(1, Suite.HEART),
+    ]
+    result = GameEvaluator.get_straight_flush_cards(hand_cards)
+    assert expected == result
+
+def test_straight_flush_sad_path_no_flush():
+    hand_cards = [
+        Card(1, Suite.HEART),
+        Card(2, Suite.HEART),
+        Card(3, Suite.HEART),
+        Card(4, Suite.HEART),
+        Card(5, Suite.DIAMOND),
+        Card(6, Suite.CLUB),
+        Card(7, Suite.SPADE),
+    ]
     expected = [
     ]
-    result = GameEvaluator.get_straight_cards(hand_cards)
+    result = GameEvaluator.get_straight_flush_cards(hand_cards)
+    assert expected == result
+
+def test_straight_flush_sad_path_no_straight():
+    hand_cards = [
+        Card(1, Suite.HEART),
+        Card(2, Suite.HEART),
+        Card(3, Suite.HEART),
+        Card(4, Suite.HEART),
+        Card(8, Suite.DIAMOND),
+        Card(10, Suite.CLUB),
+        Card(11, Suite.SPADE),
+    ]
+    expected = [
+    ]
+    result = GameEvaluator.get_straight_flush_cards(hand_cards)
+    assert expected == result
+
+def test_royal_flush_happy_path():
+    hand_cards = [
+        Card(1, Suite.HEART),
+        Card(10, Suite.HEART),
+        Card(11, Suite.HEART),
+        Card(12, Suite.HEART),
+        Card(13, Suite.HEART),
+        Card(8, Suite.HEART),
+        Card(9, Suite.HEART),
+    ]
+    expected = [
+        Card(10, Suite.HEART),
+        Card(11, Suite.HEART),
+        Card(12, Suite.HEART),
+        Card(13, Suite.HEART),
+        Card(1, Suite.HEART),
+    ]
+    result = GameEvaluator.get_royal_flush_cards(hand_cards)
+    assert expected == result
+
+def test_royal_flush_sad_path():
+    hand_cards = [
+        Card(1, Suite.HEART),
+        Card(10, Suite.HEART),
+        Card(11, Suite.SPADE),
+        Card(12, Suite.HEART),
+        Card(13, Suite.CLUB),
+        Card(8, Suite.HEART),
+        Card(9, Suite.DIAMOND),
+    ]
+    expected = [
+    ]
+    result = GameEvaluator.get_royal_flush_cards(hand_cards)
     assert expected == result
